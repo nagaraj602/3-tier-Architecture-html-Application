0ef4c69cbb6c8a682c3532b76022114c712b1653
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-06-19 17:44:39 +0530

    Update nat_gateway.tf

--- with-2-NAT-Gateway/nat_gateway.tf
+++ with-2-NAT-Gateway/nat_gateway.tf
@@ -1,12 +1,28 @@
-resource "aws_eip" "nat_eip" {
+# Elastic IPs for NAT Gateways
+resource "aws_eip" "nat_eip_1" {
   domain = "vpc"
-  tags = { Name = "nat-eip" }
+  tags   = { Name = "nat-eip-1a" }
 }
 
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
+resource "aws_eip" "nat_eip_2" {
+  domain = "vpc"
+  tags   = { Name = "nat-eip-1b" }
+}
+
+# NAT Gateway for AZ 1
+resource "aws_nat_gateway" "nat_gw_1" {
+  allocation_id = aws_eip.nat_eip_1.id
   subnet_id     = aws_subnet.public_1.id
   depends_on    = [aws_internet_gateway.igw]
 
-  tags = { Name = "three-tier-nat" }
+  tags = { Name = "three-tier-nat-1a" }
+}
+
+# NAT Gateway for AZ 2
+resource "aws_nat_gateway" "nat_gw_2" {
+  allocation_id = aws_eip.nat_eip_2.id
+  subnet_id     = aws_subnet.public_2.id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = { Name = "three-tier-nat-1b" }
 }
